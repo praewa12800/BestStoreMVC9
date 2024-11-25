576fb0ba2169570fff60986ff7dbc237cc278e12
praewa ainsuwan <praewa12800@gmail.com>
2024-11-25 08:43:30 +0700

    first commit

---
+++ BestStoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using BestStoreMVC.Models;
+using BestStoreMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestStoreMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = this.context.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+        return View();
+        }
+    }
+}
